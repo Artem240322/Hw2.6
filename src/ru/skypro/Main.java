9bfbaf6f2c091ff64bd89f8fba7319eb91e14fbe
ArtemCH240322 <sun2383@mail.ru>
2022-07-03 00:23:39 +0300

    Первый комит. Практической задание. Задание 1. Задание 2.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,8 +4,18 @@ import java.util.*;
 
 public class Main {
 
+    private static final Random RANDOM = new Random();
+
     public static void main(String[] args) {
         doTask1();
+    }
+
+    private static List<Integer> generateList (int n) {
+        List<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            boolean add = list.add(RANDOM.nextInt(0, 100));
+        }
+        return list;
     }
 
     private static void doTask1() {
@@ -19,7 +29,7 @@ public class Main {
     }
 
     private  static void doTask2() {
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 5, 5, 6, 7 ));
+        List<Integer> nums = new ArrayList<>(List.of(4, 4,  5, 5,1, 1, 2, 3,  6, 7 ));
         Set<Integer> set = new TreeSet<>();
         for (Integer i : nums) {
             if (Objects.nonNull(i) && i % 2 == 0){
