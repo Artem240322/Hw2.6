cf98ba1aa3b6891c428d8f387e0b8463bab3982b
ArtemCH240322 <sun2383@mail.ru>
2022-07-02 08:31:03 +0300

    Первый комит. Практической задание.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,29 @@
 package ru.skypro;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            list.set(i, s + "!");
+            System.out.println(s);
+        }
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+
+        Iterator var4 = list.iterator();
+
+        while (var4.hasNext()) {
+            String s = (String) var4.next();
+            System.out.println(s);
+        }
+
     }
 }
